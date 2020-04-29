42e8d23f2ee329e36594ec2fb782142db1a94f7f
David Tauraso <david318@me.com>
2020-04-29 13:59:03 -0700

    Debugging
    Co-authored-by: Lexie Jiang <lexiemingjiang@gmail.com>
    Co-authored-by: Bryan Adams <bryankeltonadams@gmail.com>

--- adventure/api.py
+++ adventure/api.py
@@ -62,7 +62,7 @@ def move(request):
         players = room.playerNames(player_id)
         return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
 
-def print_room(room):
+def get_room_data(room):
 
     return f'Title:\n{room.title}\nDescription:\n{room.description}\nDirections:\n{room.n_to} {room.s_to} {room.e_to} {room.w_to}'
 
@@ -73,7 +73,7 @@ def rooms(request):
     
     x = Room.objects.all()
     # print("Our rooms", x)
-    [print_room(room) for room in Room.objects.all()]
+    [print(get_room_data(room)) for room in Room.objects.all()]
     # for getting rooms
     # have to get the rooms(from the request?)
     return JsonResponse({'rooms': x}, safe=True)
